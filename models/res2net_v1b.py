90682efd72a328fbb78dde145124cf6fd6683c5f
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2021-11-29 20:56:25 +1100

    Update res2net_v1b.py

--- models/res2net_v1b.py
+++ models/res2net_v1b.py
@@ -203,12 +203,8 @@ def res2net50_v1b_26w_4s(pretrained=True, **kwargs):
         pretrained (bool): If True, returns a model pre-trained on ImageNet
     """
     model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth = 26, scale = 4, **kwargs)
-    torch.save(model.state_dict(),'/gdrive/MyDrive/v3.pth')
-    model.load_state_dict(torch.load("/gdrive/MyDrive/v3.pth"))
     if pretrained:
-        model.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
-        torch.save(model.state_dict(),'/gdrive/MyDrive/v3.pth')
-        pretrained = False
+        model.load_state_dict(torch.load("/gdrive/MyDrive/v3.pth"))
     return model
 
 
