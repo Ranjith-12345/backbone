aa19051c930004696752450c13dd5e358a257dd1
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2021-11-29 12:46:34 +1100

    Update backbone.py

--- models/backbone.py
+++ models/backbone.py
@@ -89,7 +89,7 @@ class Backbone(BackboneBase):
                  dilation: bool):
         backbone =res2net50_v1b_26w_4s(
             pretrained=is_main_process())
-        num_channels = 512
+        num_channels = 2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
 
