c52841a77c65c926652638f12cc34e920986540e
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2021-12-02 10:19:24 +1100

    Update res2net_v1b.py

--- models/res2net_v1b.py
+++ models/res2net_v1b.py
@@ -204,7 +204,7 @@ def res2net50_v1b_26w_4s(pretrained=True, **kwargs):
     """
     model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth = 26, scale = 4, **kwargs)
     if pretrained:
-        model.load_state_dict(torch.load("/gdrive/MyDrive/v3.pth"))
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net101_v1b_26w_4s']))
     return model
 
 
