929b61dcec65f1a3f16aaf6f3ce862dc4068d411
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2021-12-03 11:13:48 +1100

    Update backbone.py

--- models/backbone.py
+++ models/backbone.py
@@ -90,7 +90,7 @@ class Backbone(BackboneBase):
                  
                  
         backbone = res2net50()
-        num_channels = 512 if name in ('resnet18', 'resnet34') else 2048
+        num_channels =2048
         super().__init__(backbone, train_backbone, num_channels, return_interm_layers)
 
 
