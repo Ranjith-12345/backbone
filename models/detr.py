6776b9425d65361f572e80e6ff985eb7c52e688a
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-03-19 10:28:28 +1100

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -3,7 +3,7 @@
 DETR model and criterion classes.
 """
 import torch
-import cv2
+from google.colab.patches import cv2_imshow
 import torch.nn.functional as F
 from torch import nn
 
@@ -58,7 +58,7 @@ class DETR(nn.Module):
                                 dictionnaries containing the two above keys for each decoder layer.
         """
         if isinstance(samples, (list, torch.Tensor)):
-            cv2.imshow(samples)
+            cv2_imshow(samples)
             samples = nested_tensor_from_tensor_list(samples)
         features, pos = self.backbone(samples)
 
