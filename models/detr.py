f303846ceefc29f1f0b49b89b9a41a86bb8c26d6
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-03-19 11:40:39 +1100

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -37,6 +37,22 @@ class DETR(nn.Module):
         self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        self.input_proj = nn.Sequential(
+            nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
+            nn.BatchNorm2d(backbone.num_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
+            nn.BatchNorm2d(backbone.num_channels),
+        )
+
+        '''self.global_att = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(channels, inter_channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(inter_channels),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
+            nn.BatchNorm2d(channels),
+        )'''
         self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.backbone = backbone
         self.aux_loss = aux_loss
@@ -57,8 +73,6 @@ class DETR(nn.Module):
                                 dictionnaries containing the two above keys for each decoder layer.
         """
         if isinstance(samples, (list, torch.Tensor)):
-            print("hello")
-            print(samples)
             samples = nested_tensor_from_tensor_list(samples)
         features, pos = self.backbone(samples)
 
