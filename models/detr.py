f4800758ed682ff9c4c6fecf92c1beb7fe320dcf
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-03-22 14:16:24 +1100

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -37,6 +37,7 @@ class DETR(nn.Module):
         self.class_embed = nn.Linear(hidden_dim, num_classes + 1)
         self.bbox_embed = MLP(hidden_dim, hidden_dim, 4, 3)
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
+        self.input_proj = MixConv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         '''self.localatt = nn.Sequential(
             nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
             nn.BatchNorm2d(backbone.num_channels),
@@ -52,9 +53,9 @@ class DETR(nn.Module):
             nn.ReLU(inplace=False),
             nn.Conv2d(inter_channels, channels, kernel_size=1, stride=1, padding=0),
             nn.BatchNorm2d(channels),
-        )'''
-        self.input_proj = MixConv2d(backbone.num_channels, hidden_dim, kernel_size=1)
-       ''' self.x = nn.Sequential(
+        )
+        
+        self.x = nn.Sequential(
           nn.AdaptiveAvgPool2d(1),
           nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1),
           nn.BatchNorm2d(256),
