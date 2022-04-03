71a76cc63bacb12224916eb8675d63c7123352c9
Ranjith-12345 <37790731+Ranjith-12345@users.noreply.github.com>
2022-04-03 19:48:53 +1000

    Update detr.py

--- models/detr.py
+++ models/detr.py
@@ -39,7 +39,7 @@ class DETR(nn.Module):
         self.query_embed = nn.Embedding(num_queries, hidden_dim)
         #self.input_proj = nn.Conv2d(backbone.num_channels, hidden_dim, kernel_size=1)
         self.input_proj = nn.Sequential(
-            AdaptiveAvgPool2d(1),
+            nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(backbone.num_channels, backbone.num_channels, kernel_size=1),
             nn.BatchNorm2d(backbone.num_channels),
             nn.ReLU(inplace=False),
